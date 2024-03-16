f4455069bd0e36c584b68335afe8619acb323452
Harsh <harsh@gmail.com>
2024-03-16 17:21:38 +0530

    njsandnd

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const addBike = require("./routes/post/addBike");
 const bikes = require("./routes/get/bikes");
 const adminLogin = require("./routes/post/adminLogin");
 const updateAdmin = require("./routes/post/updateAdmin");
+const bike = require("./routes/get/bike");
 
 const app = express();
 
@@ -18,6 +19,7 @@ app.listen(80);
 
 app.get("/", (_, resp) => resp.send("Hello from zoom server"));
 app.get("/bikes", bikes);
+app.get("/bike/:id", bike);
 
 app.post("/login", login);
 app.post("/verify-otp", verifyOTP);
