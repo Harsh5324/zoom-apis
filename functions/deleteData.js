8433aa3616fe9732002635765097f945c4539036
Harsh <harsh@gmail.com>
2024-04-16 17:53:19 +0530

    fm;mf;k;fffg

--- functions/deleteData.js
+++ functions/deleteData.js
@@ -3,7 +3,7 @@ const db = require("./db");
 const deleteData = (_id, table) =>
   new Promise(async (resolve, reject) => {
     try {
-      await db.promise().query(`delete from '${table}' where _id = ${_id}`);
+      await db.promise().query(`delete from ${table} where _id = ${_id}`);
       resolve({ status: "SUCCESS" });
     } catch (err) {
       reject(err);
